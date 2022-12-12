3dfc4b59d66450156cdf53a2d7cc5939a37a86e1
PiyushNegi1703 <112609875+PiyushNegi1703@users.noreply.github.com>
2022-12-12 12:42:57 +0530

    3 files modified

--- promise-async.js
+++ promise-async.js
@@ -1,21 +1,44 @@
-const cookies = [{name:"Chocolate Cookies"},{name:"Macaron Cookies"}]
-const newCookie = {name:"Biscotti Cookies"};
+const cookies = [{ name: 'Chocolate Cookies' }, { name: 'Macaron Cookies' }];
+const newCookie = { name: 'Biscotti Cookies' };
 
 // Progression 1: create a function to get all the cookies
 
-   // Progression 2: using setTimeout() - use 1000 units for time parameter
+// Progression 2: using setTimeout() - use 1000 units for time parameter
+function getCookies() {
+  setTimeout(() => {
+    let output = '';
 
+    for (i = 0; i < cookies.length; i++) {
+      output += `<li> ${cookies[i].name} </li>`;
+    }
 
-//Progression 3: Create a function to creat cookies and create an object of Promise.
+    document.body.innerHTML = output;
+  }, 1000);
+}
 
+//Progression 3: Create a function to create cookies and create an object of Promise.
 
-    // Progression 4: adding two parameters and using setTimeout() - use 2000 units for time parameter
+// Progression 4: adding two parameters and using setTimeout() - use 2000 units for time parameter
 
-      
-      // Progression 5: handling errors and adding new cookie to the list
-
+// Progression 5: handling errors and adding new cookie to the list
+function createCookie(newCookie) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (newCookie.name != '') {
+        resolve(cookies.push(newCookie));
+        resolve(console.log('The Object newCookie is added to the cookies'));
+      } else {
+        reject(console.log("The Object newCookie can't be empty"));
+      }
+    }, 2000);
+  });
+}
 
 // Progression 7: creating a new async function
+async function callCookies() {
+  await createCookie(newCookie);
+  getCookies();
+}
 
 // calling the new async function
-
+callCookies();
--- promise.js
+++ promise.js
@@ -1,15 +1,43 @@
-const cookies = [{name:"Chocolate Cookies"},{name:"Macaron Cookies"}]
-const newCookie = {name:"Biscotti Cookies"};
+const cookies = [{ name: 'Chocolate Cookies' }, { name: 'Macaron Cookies' }];
+const newCookie = { name: 'Biscotti Cookies' };
 
 // Progression 1: create a function to get all the cookies
-  // Progression 2: using setTimeout() -- use 1000 units for time parameter
+// Progression 2: using setTimeout() -- use 1000 units for time parameter
+function getCookies() {
+  setTimeout(() => {
+    let output = '';
 
-//Progression 3: Create a function to creat cookies and create an object of Promise.
+    for (i = 0; i < cookies.length; i++) {
+      output += `<li> ${cookies[i].name} </li>`;
+    }
 
-    // Progression 4: adding two parameters and using setTimeout() -- use 2000 units for time parameter
-      
-      // Progression 5: handling errors and adding new cookie to the list
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+//Progression 3: Create a function to create cookies and create an object of Promise.
+
+// Progression 4: adding two parameters and using setTimeout() -- use 2000 units for time parameter
+
+// Progression 5: handling errors and adding new cookie to the list
+function createCookie(newCookie) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (newCookie.name != '') {
+        resolve(cookies.push(newCookie));
+      } else {
+        reject("The Object newCookie can't be empty");
+      }
+    }, 2000);
+  });
+}
 
 // Progression 6: call function using `.then`
-
-
+createCookie(newCookie)
+  .then(() => {
+    console.log('The newCookie has been added to cookies');
+    return getCookies();
+  })
+  .catch((message) => {
+    console.log(message);
+  });
